2b24febf7f057acc7c79d45b844927d324eeee6e
mkk <mk.kyaw@mit.com.mm>
2023-09-21 18:47:34 +0630

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import { db } from "./config/db.config";
 import { router } from "./routes/post.routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
